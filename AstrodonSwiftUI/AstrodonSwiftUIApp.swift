2ac11fde41743d837afa2268a1364b84ea5ee890
Dominik Hauser <dom@dasdom.de>
2026-04-12 19:16:12 +0200

    Initial Commit

---
+++ AstrodonSwiftUI/AstrodonSwiftUIApp.swift
@@ -0,0 +1,15 @@
+//  Created by Dominik Hauser on 12.04.26.
+//  
+//
+
+
+import SwiftUI
+
+@main
+struct AstrodonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
